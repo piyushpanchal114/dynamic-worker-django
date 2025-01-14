e605da59bf6e6b1ae2f838ca78f736ea28f7ab01
Piyush Panchal <piyushpanchal114@gmail.com>
2025-01-14 16:50:42 +0530

    .

--- core/app/urls.py
+++ core/app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("spin-worker", views.SpinWorker.as_view()),
     path("task-add", views.AddTask.as_view()),
     path("spin-job", views.SpinJob.as_view()),
+    path("deployments", views.GetDeployments.as_view()),
 ]
